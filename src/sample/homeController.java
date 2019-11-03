f8e18785e93237210e76e910963861004f70aebe
rhuthmos <rhythm18082@iiitd.ac.in>
2019-11-03 17:39:45 +0530

    Added zombie and timerbar

--- src/sample/homeController.java
+++ src/sample/homeController.java
@@ -5,19 +5,41 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class homeController {
-	public void continueGame(ActionEvent actionEvent) throws IOException {
+	private int Level = 1;
+
+	public void setLevel(int level) {
+		Level = level;
+	}
+
+
+	public void continueGame(ActionEvent actionEvent) throws IOException, InterruptedException {
+
+		if (Level==1){
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("level1.fxml"));
+			Parent root = (Parent)loader.load();
+			//((level1Controller)loader.getController()).start();
+			((Node)actionEvent.getSource()).getScene().setRoot(root);
+		}
+		else{
+			Parent root2 = FXMLLoader.load(getClass().getResource("level3.fxml"));
+			((Node)actionEvent.getSource()).getScene().setRoot(root2);
+		}
+
+	}
+
+	public void selectLevel(MouseEvent mouseEvent) throws IOException {
 		Parent root2 = FXMLLoader.load(getClass().getResource("selectLevel.fxml"));
-		//Stage myStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-		//myStage.setMaximized(true);
-		//myStage.setFullScreen(true);
-		//myStage.setScene(new Scene(root2, 300, 300));
-		//myStage.show();
-		((Node)actionEvent.getSource()).getScene().setRoot(root2);
+		((Node)mouseEvent.getSource()).getScene().setRoot(root2);
 		root2.getStylesheets().add(getClass().getResource("buttonStyle2.css").toString());
+	}
+
+	public void quitGame(MouseEvent mouseEvent) {
+		System.exit(0);
 	}
 }
--- src/sample/level1Controller.java
+++ src/sample/level1Controller.java
@@ -1,7 +1,9 @@
 package sample;
 
 import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
 import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -26,6 +28,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 
 
 public class level1Controller {
@@ -38,6 +41,11 @@ public class level1Controller {
 	public ImageView peaCard;
 	public double startDragX , startDragY;
 	public ImageView peashooter;
+	public ImageView khopdi;
+
+	public Button b_08;
+	public GridPane myGrid;
+	public Button b_00;
 
 	private String selectedImage;
 
@@ -59,6 +67,7 @@ public class level1Controller {
 		((menuController) loader.getController()).setCallingButton(PauseBtn);
 		primaryStage2.show();*/
 		menu.setVisible(true);
+		menu.toFront();
 		PauseBtn.setDisable(true);
 	}
 
@@ -99,8 +108,9 @@ public class level1Controller {
 		((Node)mouseEvent.getSource()).getScene().setRoot((Parent) FXMLLoader.load(getClass().getResource("level1.fxml")));
 	}
 
-	public void addSun(MouseEvent mouseEvent){
-		addSun(570);
+	// this function is called by our additional button add this in initialize and remove the button
+	public void addSun(MouseEvent mouseEvent) throws InterruptedException {
+		start();
 	}
 
 	public void addSun(double x){
@@ -221,6 +231,61 @@ public class level1Controller {
 	}
 	public void plantsunFlower(MouseEvent mouseEvent) {
 		selectedImage = "sample/images/plants/sunflower.gif";
+	}
+
+	public void startMeter(){
+		double xnot = khopdi.getX();
+		double ynot = khopdi.getY();
+		PathElement[] path = {
+				new MoveTo(xnot,ynot+10),
+				new LineTo(xnot-180, ynot+10),
+		};
+
+		Path road = new Path();
+		road.getElements().addAll(path);
+		PathTransition anim2 = new PathTransition();
+		anim2.setNode(khopdi);
+		anim2.setPath(road);
+		anim2.setDuration(new Duration(60000));
+		anim2.play();
+	}
+
+	public void zombiesEntry(){
+		double x1 = b_08.getLayoutX() + myParent.getScene().getWindow().getX();
+		double y1 = myGrid.getLayoutY() ;
+		Image myimg = new Image("sample/images/zombies/zombie.gif");
+		ImageView img = new ImageView();
+		img.setImage(myimg);
+		img.setLayoutX(x1);
+		img.setLayoutY(y1);
+		myParent.getChildren().add(img);
+
+
+
+		double x2 = b_00.getLayoutX() + myParent.getScene().getWindow().getX();
+
+		PathElement[] path = {
+				new MoveTo(b_08.getLayoutX() - 2.5*myParent.getScene().getWindow().getX(),b_08.getLayoutY()),
+				new LineTo(b_00.getLayoutX() - 2.5*myParent.getScene().getWindow().getX(),b_08.getLayoutY()),
+		};
+
+		Path road = new Path();
+		road.getElements().addAll(path);
+		PathTransition anim2 = new PathTransition();
+		anim2.setNode(img);
+		anim2.setPath(road);
+		anim2.setDuration(new Duration(30000));
+		anim2.play();
+
+	}
+
+	void start() throws InterruptedException {
+		startMeter();
+		for (int i=0; i<6; i++){
+			zombiesEntry();
+			// add a waiting mechanism here
+			addSun(400 + (20*i));
+		}
 	}
 
 }
